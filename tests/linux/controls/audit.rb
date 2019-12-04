7229047c6569773e214c5b5d9a2697cd33518d33
kevinwood75 <kevin.wood75@gmail.com>
2019-12-03 21:19:50 -0500

    added check for audit

---
+++ tests/linux/controls/audit.rb
@@ -0,0 +1,9 @@
+control "audit-1-5-1" do
+  impact 1.0
+  title "audit"
+  desc "audit"
+  describe file('/etc/init/serial.conf') do
+    its('content') { should_not match 'pre-start' }
+    its('group') { should eq 'kwood' }
+  end
+end
